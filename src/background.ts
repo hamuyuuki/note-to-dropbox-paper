a39889ae8a68a0df7c881983a50172ac04529bb0
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2020-10-27 10:06:22 +0900

    Authorize when the submit button is clicked

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,4 @@
 import { browser } from 'webextension-polyfill-ts';
 
 browser.runtime.onInstalled.addListener(async () => {
-  const redirectUrl = browser.identity.getRedirectURL("oauth2/callback");
-  const client_id = "";
-  const authorization_url = `https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirectUrl}&response_type=token`;
-
-  const redirect_url = await browser.identity.launchWebAuthFlow({ 'url': authorization_url, 'interactive': true });
-  console.log(redirect_url);
 });
--- src/note.tsx
+++ src/note.tsx
@@ -4,6 +4,7 @@ import { Grid, Dropdown, Button, Container, Divider, Input } from 'semantic-ui-r
 import 'semantic-ui-css/semantic.min.css'
 import { Dropbox } from 'dropbox';
 import fetch from 'isomorphic-fetch';
+import { browser } from 'webextension-polyfill-ts';
 
 const folderOptions = [
   { key: 'a', value: 'a', text: '調べ物' },
@@ -32,7 +33,14 @@ export default class App extends Component<Props, State>{
   };
 
   onClick = async (event, data) => {
-    const dropbox = new Dropbox({ fetch, accessToken: "" });
+    const redirectUrl = browser.identity.getRedirectURL("oauth2/callback");
+    const client_id = "";
+    const authorization_url = `https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirectUrl}&response_type=token`;
+
+    const redirect_url = await browser.identity.launchWebAuthFlow({ 'url': authorization_url, 'interactive': true });
+    const fragments = new URLSearchParams(new URL(redirect_url).hash.substr(1));
+
+    const dropbox = new Dropbox({ fetch, accessToken: fragments.get("access_token") });
     const content = this.state.titleValue + '\n\n' + this.state.bodyValue.replace(/\\/g, "");
     const response = await dropbox.paperDocsCreate({ contents: content, import_format: {".tag": "markdown"}, parent_folder_id: "" });
     alert("Dropbox Paperへ登録が成功しました！");
