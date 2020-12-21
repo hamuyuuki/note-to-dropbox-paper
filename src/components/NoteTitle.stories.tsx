89f7656d703a93a16854ad5c33d9bc4acfb542c4
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2020-12-21 10:35:47 +0900

    Create stories of the NoteTitle component

---
+++ src/components/NoteTitle.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NoteTitle from './NoteTitle';
+
+export default {
+  component: NoteTitle,
+  title: 'NoteTitle'
+};
+
+export const Default = (): JSX.Element => <NoteTitle />
+export const Inputed = (): JSX.Element => <NoteTitle defaultTitle="Inputed" />
--- src/components/NoteTitle.tsx
+++ src/components/NoteTitle.tsx
@@ -3,19 +3,19 @@ import { Input, InputOnChangeData } from 'semantic-ui-react'
 
 export default function NoteTitle(
   props: {
-    title: string,
-    onChange: (
+    defaultTitle?: string,
+    onChange?: (
       event: React.ChangeEvent<HTMLInputElement>,
       data: InputOnChangeData
     ) => void
   }
 ): JSX.Element {
   return <Input
-    defaultValue={props.title}
+    defaultValue={props.defaultTitle}
+    fluid
+    onChange={props.onChange}
+    placeholder='Title...'
     size='massive'
     transparent
-    fluid
-    placeholder='Title...'
-    onChange={props.onChange}
   />
 }
